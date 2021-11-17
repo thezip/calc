b5fa0e69047ca356e342acb2fa2ebe9828131d64
ZiP <thezip33@gmail.com>
2021-11-17 15:02:26 +0200

    calculating the result

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', 'X', '/'];
 
 const out = document.querySelector('.calc-screen p');
-out.textContent = 123;
 
 const clearAll = () => {
   a = '';
@@ -16,8 +15,6 @@ const clearAll = () => {
   finish = false;
   out.textContent = 0;
 }
-
-// document.querySelector('.ac').onclick = clearAll;
 
 document.querySelector('.calc-buttons').onclick = (event) => {
   if (!event.target.classList.contains('btn')) return;
@@ -30,7 +27,51 @@ document.querySelector('.calc-buttons').onclick = (event) => {
   const key = event.target.textContent;
 
   if (digit.includes(key)) {
-    a += key;
+    if (b === '' && sign === '') {
+      a += key;
+      out.textContent = a;
+    }
+    else if (a !== '' && b !== '' && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+    }
+    else {
+      b += key;
+      out.textContent = b;
+    }
+    // console.table(a, b, sign);
+    return;
+  }
+
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+  }
+
+  if (key === '=') {
+    if (b === '') b = a;
+    switch (sign) {
+      case '+':
+        a = (+a) + (+b);
+        break;
+      case '-':
+        a = a - b;
+        break;
+      case 'X':
+        a = a * b;
+        break;
+      case '/':
+        if (b === '0') {
+          out.textContent = 'Error';
+          setTimeout(clearAll, 1500);
+          return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
     out.textContent = a;
+    // console.table(a, b, sign);
   }
 }
